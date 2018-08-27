e745de040dad461f8207d20f181e501f426c21ea
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-27 20:41:42 +0530

    quotient-rem

---
+++ cspp2- practice/m1/quotient_remainder.java
@@ -0,0 +1,12 @@
+class quotient_remainder
+{
+	public static void main(String[] args)
+	{
+        int a = 10;
+        int b = 2;
+        int quotient = a/b;
+        int remainder = a%b;
+        System.out.println(quotient);
+        System.out.println(remainder);
+	}
+}
