75e34031516098a05d1289e89bc9bc4d6708fdd7
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-27 22:30:10 +0530

    Solution

---
+++ cspp2- assignments/m1/Assignment-3/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		calculateFromDegreesToFarenheit(degreesCelsius);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+	public static void calculateFromDegreesToFarenheit(double degreesCelsius)
+	{
+		double fahrenheit = degreesCelsius *1.8 +32;
+		System.out.println(fahrenheit);
+	}
+}
