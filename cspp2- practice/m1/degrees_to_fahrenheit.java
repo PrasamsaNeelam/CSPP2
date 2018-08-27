027656025b5874b9284a6ab65e3cf120aaa2715f
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-27 20:59:31 +0530

    degree to fahrenheit

---
+++ cspp2- practice/m1/degrees_to_fahrenheit.java
@@ -0,0 +1,9 @@
+class degrees_to_fahrenheit
+{
+	public static void main(String[] args)
+	{
+		float degrees = 35f;
+		float fahrenheit = (degrees * 1.8f) + 32;
+		System.out.println(fahrenheit);
+	}
+}
