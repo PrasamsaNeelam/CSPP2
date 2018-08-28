ee6d05219b80be1e4d328b3e5abd135683011197
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-28 17:35:16 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2- assignments/m2/Assignment-2/Solution.java
@@ -0,0 +1,33 @@
+/**
+* Author: Prasamsa
+* Date: 28 august 2018
+* Finds the fourth power of the given number
+*/
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public class Solution {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		rootsOfQuadraticEquation(a, b, c);
+	}
+	/*
+	Need to write the rootsOfQuadraticEquation function and print the output.
+	*/
+	static void rootsOfQuadraticEquation(int a, int b, int c)
+	{
+		double val = Math.sqrt((b*b) - 4*a*c)/(2*a);
+		double root1 = (-b) + val;
+		double root2 = (-b) - val;
+        System.out.println(root1 + "," + root2);
+	}
+}
