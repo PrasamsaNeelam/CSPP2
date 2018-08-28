b46087ac40daf4e85c4c1dea01cace16c36b6e9b
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-28 17:50:59 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2- assignments/m2/Assignment-2/Solution.java
+++ cspp2- assignments/m2/Assignment-2/Solution.java
@@ -1,36 +1,36 @@
 /**
-* Author: Prasamsa
+* Finds the fourth power of the given number.
+* Author: Prasamsa.
 * Date: 28 august 2018
-* Finds the fourth power of the given number
 */
 import java.util.Scanner;
 /**
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-		double val = Math.sqrt((b * b) - (4 * a * c));
-		double root1 = ((-b) + val) / (2 * a);
-		double root2 = ((-b) - val) / (2 * a);
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+     * { function_description }
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+        double val = Math.sqrt((b * b) - (4 * a * c));
+        double root1 = ((-b) + val) / (2 * a);
+        double root2 = ((-b) - val) / (2 * a);
         System.out.println(root1 + " " + root2);
-	}
+    }
 }
