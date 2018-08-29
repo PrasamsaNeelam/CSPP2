ed62626f8e378a358700b29515102fbe2041a601
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-29 10:30:59 +0530

    while loop

---
+++ cspp2- practice/m3/ConverttoWhileLoop.java
@@ -0,0 +1,23 @@
+/**
+ * Class for convertto while loop.
+ * Author: Prasamsa
+ * Date: 29 august 2018
+ */
+class ConverttoWhileLoop
+{
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args)
+    {
+        int i = 2;
+        while (i <= 10)
+        {
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("GoodBye!");
+    }
+}
