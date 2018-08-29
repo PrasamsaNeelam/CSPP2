63c95478e972bdf9f7007e093b7d67c629485c5f
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-29 10:51:38 +0530

    sum of n numbers

--- cspp2- practice/m3/SumOfNNumbers.java
+++ cspp2- practice/m3/SumOfNNumbers.java
@@ -1,6 +1,19 @@
+/**
+ * { item_description }.
+ * Author: Prasamsa
+ * Date: 29 august 2018
+ */
 import java.util.Scanner;
+/**
+ * Class for sum of n numbers.
+ */
 class SumOfNNumbers
 {
+    /**
+     * { main function }
+     *
+     * @param      args  The arguments
+     */
 	public static void main(String[] args)
 	{
      Scanner s = new Scanner(System.in);
