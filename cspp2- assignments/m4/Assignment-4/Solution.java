190d2938a5bbef589b54869e5d9ce9a2f4c8f8b9
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-31 15:43:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2- assignments/m4/Assignment-4/Solution.java
+++ cspp2- assignments/m4/Assignment-4/Solution.java
@@ -7,33 +7,39 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
 /*
-	Do not modify the main function 
-	*/
-	/**
-	 * { main_function }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-	}
-	static String reverseString(String s) {
-		String rev = "";
+    Do not modify the main function
+    */
+    /**
+     * { main_function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**
+     * { function to reverese the given string }.
+     *
+     * @param      s     { parameter_description: s is the given string }
+     *
+     * @return     { returns a string }
+     */
+    static String reverseString(final String s) {
+        String rev = "";
         for (int i = 0; i < s.length(); i++) {
             rev = s.charAt(i) + rev;
         }
         return rev;
-	}
+    }
 }
