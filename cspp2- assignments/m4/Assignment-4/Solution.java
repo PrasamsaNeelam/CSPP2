17b3b716f38989c760eff1f46ebc0d494e450b8b
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-31 15:38:17 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

---
+++ cspp2- assignments/m4/Assignment-4/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * { To import Scanner class }.
+ * Author: Prasamsa
+ * Date: 31 august 2018
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+/*
+	Do not modify the main function 
+	*/
+	/**
+	 * { main_function }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String reverse = reverseString(s);	
+		System.out.println(reverse);
+	}
+	static String reverseString(String s) {
+		String rev = "";
+        for (int i = 0; i < s.length(); i++) {
+            rev += s.charAt(i);
+        }
+        return rev;
+	}
+}
