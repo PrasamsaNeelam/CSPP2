074d9034c894c003ed35f32c5f3e9d4dc7d98096
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-05 18:17:48 +0530

    list interface

---
+++ cspp2- assignments/m9/Assignment-2/interface.java
@@ -0,0 +1,9 @@
+interface listInterface {
+	void add(int item);
+	int size();
+	void remove(int index);
+	int get(int index);
+	String toString();
+	boolean contains(int item);
+	int indexOf(int item);
+}
