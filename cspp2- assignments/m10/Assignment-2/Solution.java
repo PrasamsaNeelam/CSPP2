f8ac216602a5d3ce9b588d3dcc2890d03f9f7cd5
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-06 17:04:05 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2- assignments/m10/Assignment-2/Solution.java
+++ cspp2- assignments/m10/Assignment-2/Solution.java
@@ -8,20 +8,23 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
 
     }
-	
+
     /**
      * { main function to perform string operations }.
      *
      * @param      args  The arguments
      */
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl;
+        StringListInterface l = (StringListInterface) sl;
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -33,7 +36,7 @@ public class Solution {
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
                 case "add":
-	                l.add(tokens[1]);
+                    l.add(tokens[1]);
                 break;
                 case "addAll":
                 if (tokens.length == 2) {
@@ -69,5 +72,5 @@ public class Solution {
                 default:
             }
         }
-	}
+    }
 }
