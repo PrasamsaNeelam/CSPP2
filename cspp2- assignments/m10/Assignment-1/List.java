c258049d516d747603a22da9015aef4416d1d2a4
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-06 10:44:45 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2- assignments/m10/Assignment-1/List.java
+++ cspp2- assignments/m10/Assignment-1/List.java
@@ -353,7 +353,7 @@ public class List {
          * @param      item   The item
          */
     public void add(final int index, int item) {
-    	if (index >= 0) {
+    	if (index >= 0 && index < size) {
     		for (int i = size; i > index; i--) {
         	list[i] = list[i - 1];
         }
