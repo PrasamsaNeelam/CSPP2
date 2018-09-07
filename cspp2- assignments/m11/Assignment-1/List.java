c3b252f8e3fadd92785f299deb188cb7f2032c34
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-07 21:18:41 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2- assignments/m11/Assignment-1/List.java
+++ cspp2- assignments/m11/Assignment-1/List.java
@@ -405,7 +405,7 @@ public class List {
                     if (tokens.length == 2) {
                         String[] t1 = tokens[1].split(",");
                         int[] temp = new int[t1.length];
-                        for(int i = 0; i < temp.length; i++) {
+                        for (int i = 0; i < temp.length; i++) {
                             temp[i] = Integer.parseInt(t1[i]);
                         }
                         l.addAll(temp);
@@ -415,13 +415,15 @@ public class List {
                     if (tokens.length == 2) {
                         String[] t2 = tokens[1].split(",");
                         int[] a = new int[t2.length];
-                        for(int i = 0; i < t2.length; i++)
+                        for (int i = 0; i < t2.length; i++)
                             a[i] = Integer.parseInt(t2[i]);
                         l.removeAll(a);
                     }
                 break;
                 case "subList":
-                    if (tokens.length != 2) break;
+                    if (tokens.length != 2) {
+                        break;
+                    }
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(Integer.parseInt(arrstring3[0]),
                             Integer.parseInt(arrstring3[1]));
@@ -429,12 +431,12 @@ public class List {
                         System.out.println(object);
                     }
                     break;
-                
+
                 case "equals":
                     if (tokens.length == 2) {
                         String[] lt = tokens[1].split(",");
                         List l2 = new List();
-                        for (int k = 0; k < lt.length; k++ ) {
+                        for (int k = 0; k < lt.length; k++) {
                             l2.add(Integer.parseInt(lt[k]));
                         }
                         System.out.println(l.equals(l2));
@@ -449,4 +451,6 @@ public class List {
         }
     }
 }
+
+
 
