88dfeed5018af97ae6fc36c3ee01a37aa4d31ea7
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-13 17:56:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2- assignments/m16/bookyourshow/BookYourShow.java
+++ cspp2- assignments/m16/bookyourshow/BookYourShow.java
@@ -42,7 +42,7 @@ class Show {
 /**
  * Class for book your show.
  */
-class BookYourShow {
+public class BookYourShow {
     public Show[] allshows;
     public int allshowscount;
     public Patron[] allPatrons;
@@ -158,72 +158,6 @@ class BookYourShow {
             str1 += "]";
             str += str1;
             System.out.println(str);
-        }
-    }
-}
-/**
- * { Final class solution }
- */
-public final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() {
-
-    }
-    /**
-     * main method to drive program.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        BookYourShow bys = new BookYourShow();
-        Scanner scan = new Scanner(System.in);
-        int testCases = Integer.parseInt(scan.nextLine());
-        for (int i = 0; i < testCases; i++) {
-            String[] tokens = scan.nextLine().
-                replace("[", "").replace("]", "").split(",");
-            String[] check = tokens[0].split(" ");
-            switch (check[0]) {
-                case "add":
-                    int k = 2;
-                    String[] seats = new String[tokens.length - 2];
-                    for (int j = 0; j < seats.length; j++) {
-                        seats[j] = tokens[k++];
-                    }
-                    bys.addAShow(new Show(check[1], tokens[1], seats));
-                break;
-
-                case "book":
-                    k = 2 + 2;
-                    seats = new String[tokens.length - 2 - 2];
-                    for (int j = 0; j < seats.length; j++) {
-                        seats[j] = tokens[k++];
-                    }
-                    bys.bookAShow(check[1], tokens[1],
-                        new Patron(tokens[2], tokens[2 + 1]), seats);
-                break;
-
-                case "get":
-                    Show show = bys.getAShow(check[1], tokens[1]);
-                    if (show != null) {
-                       System.out.println(show.movieName + "," + show.showDateandTime);
-                    } else {
-                        System.out.println("No show");
-                    }
-                break;
-
-                case "print":
-                    bys.printTicket(check[1], tokens[1], tokens[2]);
-                break;
-
-                case "showAll":
-                    bys.showAll();
-                break;
-
-                default:
-                break;
-            }
         }
     }
 }
